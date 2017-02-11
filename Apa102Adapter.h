dee9398105f444efd1a3a461598b4207ea99dc0b
Ben Hencke <brainstar@gmail.com>
2017-02-10 18:12:25 -0800

    use raw SPI registers, cleanup

--- Apa102Adapter.h
+++ Apa102Adapter.h
@@ -12,14 +12,14 @@
 //borrowed from Adafruit
 // Color-order flag for LED pixels (optional extra parameter to constructor):
 // Bits 0,1 = R index (0-2), bits 2,3 = G index, bits 4,5 = B index
-#define APA102_RGB (2 | (1 << 2) | (0 << 4))
-#define APA102_RBG (2 | (2 << 2) | (1 << 4))
-#define APA102_GRB (1 | (0 << 2) | (0 << 4))
-#define APA102_GBR (0 | (0 << 2) | (1 << 4))
-#define APA102_BRG (1 | (2 << 2) | (2 << 4))
-#define APA102_BGR (0 | (1 << 2) | (2 << 4))
+#define APA102_RGB (0 | (1 << 2) | (2 << 4))
+#define APA102_RBG (0 | (2 << 2) | (1 << 4))
+#define APA102_GRB (1 | (0 << 2) | (2 << 4))
+#define APA102_GBR (2 | (0 << 2) | (1 << 4))
+#define APA102_BRG (1 | (2 << 2) | (0 << 4))
+#define APA102_BGR (2 | (1 << 2) | (0 << 4))
 
-typedef std::function<void(uint16_t index, uint8_t rgbv[])> pixelCallback;
+typedef std::function<void(uint16_t index, uint8_t rgbv[])> ApaPixelFunction;
 
 class Apa102Adapter {
 public:
@@ -36,6 +36,11 @@ public:
         SPI.setFrequency(spiFrequency);
         SPI.setBitOrder(MSBFIRST);
         SPI.setDataMode(SPI_MODE0);
+        //borrowed from SPI.cpp, set registers for a 32bit transfer buffer
+        uint16_t bits = 32;
+        const uint32_t mask = ~((SPIMMOSI << SPILMOSI) | (SPIMMISO << SPILMISO));
+        bits--;
+        SPI1U1 = ((SPI1U1 & mask) | ((bits << SPILMOSI) | (bits << SPILMISO)));
     }
 
     void setSpiFrequency(uint32_t spiFrequency) {
@@ -43,44 +48,49 @@ public:
     }
 
     void setColorOrder(uint8_t o) {
-        rOffset = (uint8_t) (o & 3);
-        gOffset = (uint8_t) ((o >> 2) & 3);
-        bOffset = (uint8_t) ((o >> 4) & 3);
+        rOffset = (uint8_t) (1+(o & 3));
+        gOffset = (uint8_t) (1+((o >> 2) & 3));
+        bOffset = (uint8_t) (1+((o >> 4) & 3));
     }
 
-    void show(uint16_t numPixels, pixelCallback cb) {
-        int i;
+    void show(uint16_t numPixels, ApaPixelFunction cb) {
+        int curPixel;
         union {
             uint32_t frame;
             uint8_t b[4];
         } buf, rgbv;
 
         //start frame
-        SPI.write32(0);
+        write32(0);
 
         //pixels, sourced from callback
-        for (i = 0; i < numPixels; i++) {
+        for (curPixel = 0; curPixel < numPixels; curPixel++) {
             rgbv.frame = 0x1f000000; //default to brightest black
-            cb(i,rgbv.b);
+            cb(curPixel,rgbv.b);
 
             //pixel brightness (5 bits)
-            buf.b[3] = (uint8_t) (rgbv.b[3] | 0xe0);
+            buf.b[0] = (uint8_t) (rgbv.b[3] | 0xe0);
 
             //swap around rgb values based on mapping
             buf.b[rOffset] = rgbv.b[0];
             buf.b[gOffset] = rgbv.b[1];
             buf.b[bOffset] = rgbv.b[2];
-            SPI.write32(buf.frame);
+            write32(buf.frame);
         }
 
         //end frame
-        //reports differ as to how many end frame bits are required
-        //since the strip is continuously updated in my app, I don't mind if some pixels lag slightly, e.g. are updated once the strip starts drawing the next update.
-        //also instead of writing all ones, effectively a white pixel, draw a black pixel.
-        SPI.write32(0xe0000000);
+        uint8_t extraShifts = (uint8_t) (1 + numPixels >> 5);
+        do {
+            write32(0xffffffff);
+        } while (--extraShifts > 0);
     }
 
 private:
+    inline void write32(uint32_t v) {
+        while(SPI1CMD & SPIBUSY) {}
+        SPI1W0 = v;
+        SPI1CMD |= SPIBUSY;
+    }
     uint8_t
             rOffset,                                // Index of red in 3-byte pixel
             gOffset,                                // Index of green byte
