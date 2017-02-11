adb02f34c2b678a6a9b197c7bc0fb24de7347ebf
Ben Hencke <brainstar@gmail.com>
2017-02-11 10:43:53 -0800

    update example for api changes

--- examples/strandtest/strandtest.ino
+++ examples/strandtest/strandtest.ino
@@ -4,15 +4,16 @@
 #define NUM_PIXELS 30
 
 Apa102Adapter strip;
-//Apa102Adapter strip(APA102_RGB, 1000000L); // change color order to RGB and data speed to 1MHz
+//Apa102Adapter strip(APA102_RGB); // change color order to RGB
 
 void setup() {
     strip.begin();
+//    strip.begin(8000000L); // change the data speed to 8MHz
 }
 
 void loop() {
     for (int counter = 0; counter < NUM_PIXELS; counter++) {
-        strip.show(NUM_PIXELS, [](uint16_t index, uint8_t rgbv[]) {
+        strip.show(NUM_PIXELS, [counter](uint16_t index, uint8_t rgbv[]) {
             //if color order is set right, this should show a bright red pixel on dim green background
             if (counter == index) {
                 rgbv[0] = 255;
